ab847d0f7faa0a4ed49442023e11d486dc4e0f03
jakob.schoenicke@arcor.de <gitterino15>
2019-08-15 15:02:53 +0200

    Added ToolWindow Skeleton

---
+++ src/main/java/TimelineToolWindow.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class TimelineToolWindow implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+    }
+
+}
